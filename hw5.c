647185023c6b9cbbc993cb1f9a2abb2cd88a5b24
LeeDyol <90294252+LeeDyol@users.noreply.github.com>
2021-11-05 04:49:03 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5];
+	int i;
+	printf("5개의 정수를 입력하세요: ");
+	scanf_s("%d" "%d" "%d" "%d" "%d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+
+	printf("홀수 출력: ");
+	for (i = 0;i < 5;i++)
+	{
+		if (arr[i] % 2 != 0)
+			printf("%d ", arr[i]);
+	}
+	printf("\n짝수 출력: ");
+	for (i = 0;i < 5;i++)
+	{
+		if (arr[i] % 2 == 0)
+			printf("%d ", arr[i]);
+	}
+	return 0;
+}
