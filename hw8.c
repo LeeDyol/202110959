d88cc26c29a7731b07cfebe247709198c818453f
LeeDyol <90294252+LeeDyol@users.noreply.github.com>
2021-11-26 05:14:35 +0900

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+char convCase(char sentence[])
+{
+	int len;
+	len = strlen(sentence);
+	const int diff = 'a' - 'A';
+	for (int i = 0; i < len; i++)
+		if (sentence[i] >= 'A' && sentence[i] <= 'Z')
+			putchar(sentence[i] + diff);
+		else if (sentence[i] >= 'a' && sentence[i] <= 'z')
+			putchar(sentence[i] - diff);
+		else if (sentence[i] == " ")
+			printf(" ");
+		else
+			return -1;
+}
+
+int main(void)
+{
+	char sentence[40] = "0";
+	char ch = 0;
+	printf("Input: ");
+	fgets(sentence, sizeof(sentence), stdin);
+	ch = convCase(sentence);
+	if (ch == -1)
+	{
+		puts("범위를 벗어난 입력입니다. \n");
+		return -1;
+	}
+	printf("Output: ");
+	return 0;
+}
