43fe9fdb5bae846d1c03c0eaaa7d82e01c70d98d
LeeDyol <90294252+LeeDyol@users.noreply.github.com>
2021-11-26 04:24:28 +0900

    Add files via upload

---
+++ practice_21_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int convCase(int ch)
+{
+	const int diff = 'a' - 'A';
+	if (ch >= 'A' && ch <= 'Z')
+		return ch + diff;
+	else if (ch >= 'a' && ch <= 'z')
+		return ch - diff;
+	else
+		return -1;
+}
+
+int main(void)
+{
+	int ch;
+	printf("문자 입력: ");
+	ch = getchar();
+	ch = convCase(ch);
+	if (ch == -1)
+	{
+		puts("범위를 벗어난 입력입니다. \n");
+		return -1;
+	}
+	putchar(ch);
+	return 0;
+}
