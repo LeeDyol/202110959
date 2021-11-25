b8f0514173f40ce06390497ef2878bae256df947
LeeDyol <90294252+LeeDyol@users.noreply.github.com>
2021-11-26 04:24:39 +0900

    Add files via upload

---
+++ practice_21_2_01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int convTolnt(char c)
+{
+	const int diff = 1 - '1';
+	return c + diff;
+}
+
+int main(void)
+{
+	char str[50];
+	int len, sum = 0;
+	printf("문자열 입력: ");
+	fgets(str, sizeof(str), stdin);
+	len = strlen(str);
+	for (int i = 0; i < len; i++)
+		if ('1' <= str[i] && str[i] <= '9')
+			sum += convTolnt(str[i]);
+	printf("숫자의 총 합: %d \n", sum);
+	return 0;
+}
