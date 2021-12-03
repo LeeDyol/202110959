9b495de679c9a6e50b77c714ed89fe250598ff5f
LeeDyol <90294252+LeeDyol@users.noreply.github.com>
2021-12-03 17:48:39 +0900

    Add files via upload

---
+++ practice_23_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct point {
+	int xpos;
+	int ypos;
+} Point;
+
+void SwapPoint(Point* ptr1, Point* ptr2)
+{
+	Point swap = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = swap;
+}
+
+int main(void)
+{
+	Point pos1 = { 2, 4 };
+	Point pos2 = { 5, 7 };
+
+	SwapPoint(&pos1, &pos2);
+	printf("[%d, %d] \n", pos1.xpos, pos1.ypos);
+	printf("[%d, %d] \n", pos2.xpos, pos2.ypos);
+	return 0;
+}
